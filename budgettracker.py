fa88b118306e1fc1b809b8ea82f0aa3f65275736
Yasmine Reis <yasminereis@Yasmines-MBP.lan>
2022-01-08 21:18:41 -0600

    first commit

---
+++ budgettracker.py
@@ -0,0 +1,33 @@
+class Budget:
+    def __init__(self, base_income = 0, base_charges=None):
+        if base_charges is None:
+            base_charges = {}
+        self.base_charges = base_charges
+        self.income = base_income
+
+    def __repr__(self):
+        to_return = ""
+        to_return += ("total income: " + str(self.income) + "\n")
+        charges = ""
+        for key, value in self.base_charges.items():
+            charges += str(key) + ": " + str(value) + "\n"
+        to_return += ("charges:\n" + charges)
+        return to_return
+
+    def adjust_charge(self, item, amount):
+        if item in self.base_charges:
+            self.base_charges[item] += amount
+        else: self.base_charges[item] = amount
+
+    def add_income(self, amount):
+        self.income += amount
+
+
+june_budget = Budget(3000)
+june_budget.adjust_charge("food", 100)
+june_budget.adjust_charge("food", 300)
+june_budget.adjust_charge("doggies", 500)
+june_budget.add_income(800)
+
+
+print(june_budget)
